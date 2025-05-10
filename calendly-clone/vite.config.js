87b7c5747194be4eefebc6b2ce5cd833cedfd223
Devon Zuegel <devonzuegel@gmail.com>
2025-05-10 12:20:54 -0400

    wip: CORS issue for the js file

--- calendly-clone/vite.config.js
+++ calendly-clone/vite.config.js
@@ -3,6 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Use relative paths instead of absolute
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
   server: {
     port: 3000,
     open: true
