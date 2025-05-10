c53bbcb9d4890b644d1264283a02cd3580082645
Devon Zuegel <devonzuegel@gmail.com>
2025-05-10 12:40:05 -0400

    tmp tweak to make sure it works

--- calendly-clone/dev/src/App.jsx
+++ calendly-clone/dev/src/App.jsx
@@ -4,7 +4,7 @@ import CalendarView from './components/CalendarView';
 function App() {
   return (
     <div className="container">
-      <div className="info-container">
+      <div className="info-container" style={{ border: '10px solid red', padding: '20px', borderRadius: '8px' }}>
         <p>If a time shows up on the calendar below, it means Devon is <i>probably</i> available for a call at that time.</p>
         <p>
           Send a calendar invite with context to <a href="mailto:devon@esmeralda.org">devon@esmeralda.org</a>, and she'll confirm ASAP!
