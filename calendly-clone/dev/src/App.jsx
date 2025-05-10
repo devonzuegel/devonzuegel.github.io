8cab8a8a243c42737c8201c14d1096925eb54e03
Devon Zuegel <devonzuegel@gmail.com>
2025-05-10 12:43:57 -0400

    tmp tweak to make sure it works

--- calendly-clone/dev/src/App.jsx
+++ calendly-clone/dev/src/App.jsx
@@ -4,6 +4,12 @@ import CalendarView from './components/CalendarView';
 function App() {
   return (
     <div className="container">
+      <div>This is a test!</div>
+      <div>This is a test!</div>
+      <div>This is a test!</div>
+      <div>This is a test!</div>
+      <div>This is a test!</div>
+      <br/>
       <div className="info-container" style={{ border: '10px solid red', padding: '20px', borderRadius: '8px' }}>
         <p>If a time shows up on the calendar below, it means Devon is <i>probably</i> available for a call at that time.</p>
         <p>
