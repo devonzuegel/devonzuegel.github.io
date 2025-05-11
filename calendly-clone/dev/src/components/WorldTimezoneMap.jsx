eae0a838f0a091faa41d077d53fbf40523596d3a
Devon Zuegel <devonzuegel@gmail.com>
2025-05-10 23:36:20 -0400

    wip

--- calendly-clone/dev/src/components/WorldTimezoneMap.jsx
+++ calendly-clone/dev/src/components/WorldTimezoneMap.jsx
@@ -8,61 +8,61 @@ const TIMEZONE_REGIONS = [
     name: 'Pacific Time',
     abbreviation: 'PT',
     timezones: ['America/Los_Angeles', 'America/Vancouver', 'America/Tijuana'],
-    path: 'M 45,47 L 48,45 L 51,46 L 52,49 L 52,55 L 51,60 L 49,65 L 46,68 L 41,68 L 39,65 L 39,60 L 40,54 L 42,50 L 45,47',
+    path: 'M 43,47 L 48,43 L 51,43 L 52,46 L 52,51 L 52,55 L 51,60 L 49,65 L 46,68 L 41,68 L 39,65 L 38,60 L 39,54 L 41,50 L 43,47',
     labelX: 45,
-    labelY: 61
+    labelY: 56
   },
   {
     id: 'mountain',
     name: 'Mountain Time',
     abbreviation: 'MT',
     timezones: ['America/Denver', 'America/Edmonton', 'America/Phoenix'],
-    path: 'M 51,46 L 55,45 L 58,46 L 59,49 L 60,55 L 60,62 L 60,68 L 55,68 L 51,68 L 46,68 L 49,65 L 51,60 L 52,55 L 52,49 L 51,46',
+    path: 'M 51,43 L 55,42 L 58,42 L 59,45 L 60,51 L 60,55 L 60,62 L 60,68 L 55,68 L 51,68 L 46,68 L 49,65 L 51,60 L 52,55 L 52,51 L 52,46 L 51,43',
     labelX: 55,
-    labelY: 61
+    labelY: 56
   },
   {
     id: 'central',
     name: 'Central Time',
     abbreviation: 'CT',
     timezones: ['America/Chicago', 'America/Mexico_City', 'America/Winnipeg'],
-    path: 'M 58,46 L 63,45 L 67,47 L 69,51 L 70,57 L 70,62 L 70,68 L 65,68 L 60,68 L 60,62 L 60,55 L 59,49 L 58,46',
+    path: 'M 58,42 L 63,41 L 67,42 L 69,46 L 70,51 L 70,55 L 70,62 L 70,68 L 65,68 L 60,68 L 60,62 L 60,55 L 60,51 L 59,45 L 58,42',
     labelX: 65,
-    labelY: 59
+    labelY: 56
   },
   {
     id: 'eastern',
     name: 'Eastern Time',
     abbreviation: 'ET',
     timezones: ['America/New_York', 'America/Toronto', 'America/Indiana/Indianapolis'],
-    path: 'M 67,47 L 71,45 L 74,46 L 77,49 L 78,54 L 79,59 L 80,65 L 80,68 L 75,68 L 70,68 L 70,62 L 70,57 L 69,51 L 67,47',
+    path: 'M 67,42 L 71,40 L 74,41 L 76,45 L 77,51 L 78,55 L 79,59 L 80,65 L 80,68 L 75,68 L 70,68 L 70,62 L 70,55 L 70,51 L 69,46 L 67,42',
     labelX: 75,
-    labelY: 59
+    labelY: 56
   },
   {
     id: 'atlantic',
     name: 'Atlantic Time',
     abbreviation: 'AT',
     timezones: ['America/Halifax', 'America/Puerto_Rico', 'America/Caracas'],
-    path: 'M 74,46 L 79,45 L 84,47 L 86,50 L 88,56 L 90,62 L 90,68 L 85,68 L 80,68 L 80,65 L 79,59 L 78,54 L 77,49 L 74,46',
-    labelX: 85,
-    labelY: 59
+    path: 'M 74,41 L 79,40 L 84,41 L 87,46 L 88,52 L 89,56 L 90,62 L 90,68 L 85,68 L 80,68 L 80,65 L 79,59 L 78,55 L 77,51 L 76,45 L 74,41',
+    labelX: 84,
+    labelY: 56
   },
   {
     id: 'alaska',
     name: 'Alaska',
     abbreviation: 'AKT',
     timezones: ['America/Anchorage'],
-    path: 'M 30,50 L 33,48 L 36,48 L 39,50 L 40,54 L 39,58 L 37,60 L 34,62 L 32,61 L 30,58 L 30,54 L 30,50',
-    labelX: 35,
-    labelY: 56
+    path: 'M 25,42 L 28,40 L 32,42 L 35,45 L 37,47 L 39,50 L 40,54 L 39,58 L 38,60 L 36,62 L 33,63 L 30,62 L 27,60 L 25,57 L 24,52 L 25,47 L 25,42',
+    labelX: 32,
+    labelY: 52
   },
   {
     id: 'hawaii',
     name: 'Hawaii',
     abbreviation: 'HST',
     timezones: ['Pacific/Honolulu'],
-    path: 'M 20,68 L 22,66 L 25,65 L 28,66 L 30,68 L 31,71 L 28,74 L 24,74 L 20,73 L 20,70 L 20,68',
+    path: 'M 20,68 L 22,65 L 24,64 L 26,65 L 28,66 L 30,68 L 31,71 L 30,73 L 28,74 L 24,74 L 21,73 L 20,71 L 20,68',
     labelX: 25,
     labelY: 70
   },
@@ -73,8 +73,8 @@ const TIMEZONE_REGIONS = [
     name: 'Mexico',
     abbreviation: 'CST/MST',
     timezones: ['America/Mexico_City', 'America/Chihuahua', 'America/Cancun'],
-    path: 'M 52,68 L 55,68 L 58,69 L 60,70 L 63,71 L 62,73 L 60,75 L 58,76 L 55,78 L 52,77 L 50,76 L 49,73 L 50,70 L 52,68',
-    labelX: 56,
+    path: 'M 50,68 L 55,68 L 60,68 L 61,70 L 62,72 L 62,74 L 60,75 L 57,76 L 54,77 L 51,77 L 49,76 L 48,73 L 49,70 L 50,68',
+    labelX: 55,
     labelY: 73
   },
   {
@@ -82,17 +82,17 @@ const TIMEZONE_REGIONS = [
     name: 'Central America',
     abbreviation: 'CST',
     timezones: ['America/Guatemala', 'America/Panama', 'America/Costa_Rica'],
-    path: 'M 60,75 L 62,73 L 65,74 L 68,76 L 70,79 L 70,82 L 68,84 L 65,85 L 62,84 L 60,82 L 59,79 L 60,75',
+    path: 'M 60,75 L 62,74 L 64,74 L 67,75 L 69,77 L 70,80 L 68,82 L 66,84 L 63,84 L 61,83 L 60,79 L 60,75',
     labelX: 65,
-    labelY: 80
+    labelY: 79
   },
   {
     id: 'caribbean',
     name: 'Caribbean',
     abbreviation: 'EST/AST',
     timezones: ['America/Havana', 'America/Santo_Domingo', 'America/Port_of_Spain'],
-    path: 'M 67,70 L 72,69 L 76,70 L 80,71 L 83,73 L 81,75 L 78,76 L 74,77 L 70,76 L 68,74 L 67,72 L 67,70',
-    labelX: 74,
+    path: 'M 67,70 L 70,69 L 75,70 L 80,71 L 83,73 L 82,75 L 78,76 L 74,77 L 71,77 L 69,75 L 67,72 L 67,70',
+    labelX: 75,
     labelY: 73
   },
   {
@@ -100,26 +100,26 @@ const TIMEZONE_REGIONS = [
     name: 'Brazil East',
     abbreviation: 'BRT',
     timezones: ['America/Sao_Paulo', 'America/Rio_de_Janeiro', 'America/Belem'],
-    path: 'M 76,85 L 80,84 L 85,85 L 89,88 L 90,92 L 89,95 L 86,98 L 82,100 L 77,97 L 76,93 L 75,89 L 76,85',
-    labelX: 82,
-    labelY: 92
+    path: 'M 77,85 L 80,83 L 85,83 L 89,86 L 91,90 L 90,94 L 87,97 L 83,99 L 79,98 L 77,96 L 76,92 L 75,88 L 77,85',
+    labelX: 84,
+    labelY: 91
   },
   {
     id: 'brazil_west',
     name: 'Brazil West',
     abbreviation: 'AMT',
     timezones: ['America/Manaus', 'America/Campo_Grande'],
-    path: 'M 66,85 L 70,84 L 74,85 L 76,85 L 75,89 L 76,93 L 77,97 L 74,98 L 70,97 L 67,95 L 65,92 L 65,88 L 66,85',
-    labelX: 70,
-    labelY: 92
+    path: 'M 67,85 L 70,83 L 74,84 L 77,85 L 75,88 L 76,92 L 77,96 L 74,97 L 71,96 L 68,94 L 66,91 L 66,88 L 67,85',
+    labelX: 71,
+    labelY: 90
   },
   {
     id: 'argentina',
     name: 'Argentina',
     abbreviation: 'ART',
     timezones: ['America/Argentina/Buenos_Aires', 'America/Argentina/Cordoba', 'America/Argentina/Mendoza'],
-    path: 'M 73,98 L 77,97 L 82,100 L 80,103 L 77,106 L 73,108 L 70,110 L 68,108 L 69,105 L 70,102 L 72,100 L 73,98',
-    labelX: 75,
+    path: 'M 72,98 L 75,97 L 79,98 L 81,100 L 80,104 L 77,107 L 73,109 L 70,110 L 69,107 L 70,103 L 71,100 L 72,98',
+    labelX: 76,
     labelY: 104
   },
   {
@@ -127,17 +127,17 @@ const TIMEZONE_REGIONS = [
     name: 'Chile',
     abbreviation: 'CLT',
     timezones: ['America/Santiago', 'America/Punta_Arenas'],
-    path: 'M 65,98 L 67,95 L 70,97 L 73,98 L 72,100 L 70,102 L 69,105 L 68,108 L 70,110 L 67,112 L 64,111 L 63,108 L 64,104 L 65,100 L 65,98',
+    path: 'M 64,95 L 66,92 L 68,94 L 71,96 L 74,97 L 72,98 L 71,100 L 70,103 L 69,107 L 70,110 L 68,113 L 66,112 L 64,110 L 63,106 L 63,100 L 64,95',
     labelX: 67,
-    labelY: 105
+    labelY: 104
   },
   {
     id: 'andes',
     name: 'Andes',
     abbreviation: 'PET',
     timezones: ['America/Lima', 'America/Bogota', 'America/La_Paz'],
-    path: 'M 55,85 L 58,83 L 62,84 L 65,85 L 65,88 L 65,92 L 65,98 L 65,100 L 64,104 L 63,108 L 60,104 L 58,100 L 56,94 L 55,90 L 55,85',
-    labelX: 60,
+    path: 'M 57,82 L 60,80 L 63,84 L 66,84 L 67,85 L 66,88 L 66,91 L 66,92 L 64,95 L 63,100 L 63,106 L 62,108 L 59,105 L 57,101 L 56,96 L 56,90 L 57,86 L 57,82',
+    labelX: 62,
     labelY: 95
   },
 
@@ -147,36 +147,36 @@ const TIMEZONE_REGIONS = [
     name: 'Western Europe',
     abbreviation: 'GMT/BST',
     timezones: ['Europe/London', 'Europe/Lisbon', 'Europe/Dublin'],
-    path: 'M 95,50 L 98,48 L 101,47 L 104,48 L 105,51 L 106,54 L 105,57 L 103,60 L 100,62 L 96,63 L 93,60 L 92,57 L 93,53 L 95,50',
+    path: 'M 93,48 L 96,46 L 99,45 L 102,46 L 104,49 L 105,52 L 105,55 L 103,58 L 100,60 L 97,61 L 94,59 L 93,56 L 92,53 L 93,48',
     labelX: 99,
-    labelY: 55
+    labelY: 53
   },
   {
     id: 'central_europe',
     name: 'Central Europe',
     abbreviation: 'CET/CEST',
     timezones: ['Europe/Paris', 'Europe/Berlin', 'Europe/Madrid', 'Europe/Rome'],
-    path: 'M 104,48 L 107,47 L 110,47 L 113,48 L 115,51 L 115,54 L 114,57 L 112,60 L 109,62 L 106,63 L 103,60 L 105,57 L 106,54 L 105,51 L 104,48',
-    labelX: 110,
-    labelY: 55
+    path: 'M 102,46 L 106,45 L 110,45 L 113,47 L 114,50 L 114,53 L 113,56 L 111,59 L 108,60 L 105,60 L 103,58 L 105,55 L 105,52 L 104,49 L 102,46',
+    labelX: 108,
+    labelY: 53
   },
   {
     id: 'eastern_europe',
     name: 'Eastern Europe',
     abbreviation: 'EET/EEST',
     timezones: ['Europe/Helsinki', 'Europe/Bucharest', 'Europe/Athens', 'Europe/Sofia'],
-    path: 'M 113,48 L 116,47 L 119,47 L 122,49 L 123,52 L 123,56 L 122,59 L 120,62 L 116,63 L 112,60 L 114,57 L 115,54 L 115,51 L 113,48',
-    labelX: 120,
-    labelY: 55
+    path: 'M 113,47 L 117,45 L 121,46 L 123,49 L 124,52 L 124,56 L 122,59 L 119,61 L 115,61 L 111,59 L 113,56 L 114,53 L 114,50 L 113,47',
+    labelX: 118,
+    labelY: 53
   },
   {
     id: 'russia_west',
     name: 'Russia West',
     abbreviation: 'MSK',
     timezones: ['Europe/Moscow', 'Europe/Kaliningrad'],
-    path: 'M 122,49 L 125,43 L 130,41 L 135,40 L 139,43 L 140,48 L 138,52 L 136,56 L 133,59 L 128,60 L 123,56 L 123,52 L 122,49',
+    path: 'M 123,45 L 126,41 L 130,39 L 135,38 L 139,40 L 141,43 L 140,48 L 138,52 L 135,55 L 131,58 L 126,58 L 124,56 L 124,52 L 123,49 L 123,45',
     labelX: 132,
-    labelY: 50
+    labelY: 49
   },
 
   // Africa
@@ -185,16 +185,16 @@ const TIMEZONE_REGIONS = [
     name: 'North Africa',
     abbreviation: 'CET/EET',
     timezones: ['Africa/Cairo', 'Africa/Tunis', 'Africa/Algiers', 'Africa/Casablanca'],
-    path: 'M 96,63 L 100,62 L 106,63 L 109,62 L 116,63 L 120,64 L 122,67 L 120,70 L 118,72 L 112,74 L 105,74 L 100,73 L 97,71 L 95,68 L 96,63',
-    labelX: 109,
-    labelY: 68
+    path: 'M 94,59 L 97,61 L 100,61 L 105,61 L 108,61 L 113,62 L 119,62 L 123,64 L 121,68 L 117,71 L 112,73 L 107,73 L 102,72 L 97,71 L 94,68 L 93,64 L 94,59',
+    labelX: 107,
+    labelY: 67
   },
   {
     id: 'west_africa',
     name: 'West Africa',
     abbreviation: 'WAT',
     timezones: ['Africa/Lagos', 'Africa/Accra', 'Africa/Dakar'],
-    path: 'M 90,70 L 93,68 L 95,68 L 97,71 L 100,73 L 101,76 L 101,79 L 98,82 L 95,84 L 92,82 L 90,78 L 89,73 L 90,70',
+    path: 'M 88,69 L 91,67 L 94,68 L 97,71 L 102,72 L 101,75 L 100,78 L 98,81 L 94,84 L 91,82 L 89,78 L 88,74 L 88,69',
     labelX: 95,
     labelY: 76
   },
@@ -203,26 +203,26 @@ const TIMEZONE_REGIONS = [
     name: 'Central Africa',
     abbreviation: 'CAT',
     timezones: ['Africa/Kinshasa', 'Africa/Khartoum', 'Africa/Bangui'],
-    path: 'M 100,73 L 105,74 L 112,74 L 111,77 L 110,80 L 106,82 L 101,83 L 98,82 L 101,79 L 101,76 L 100,73',
-    labelX: 105,
-    labelY: 79
+    path: 'M 102,72 L 107,73 L 112,73 L 111,76 L 109,79 L 105,82 L 100,82 L 97,81 L 98,78 L 100,75 L 102,72',
+    labelX: 106,
+    labelY: 77
   },
   {
     id: 'east_africa',
     name: 'East Africa',
     abbreviation: 'EAT',
     timezones: ['Africa/Nairobi', 'Africa/Addis_Ababa', 'Africa/Kampala'],
-    path: 'M 112,74 L 118,72 L 120,70 L 122,72 L 121,75 L 120,78 L 118,81 L 116,83 L 112,82 L 110,80 L 111,77 L 112,74',
+    path: 'M 112,73 L 117,71 L 120,69 L 123,70 L 124,74 L 123,79 L 118,81 L 113,81 L 109,79 L 111,76 L 112,73',
     labelX: 117,
-    labelY: 78
+    labelY: 76
   },
   {
     id: 'south_africa',
     name: 'South Africa',
     abbreviation: 'SAST',
     timezones: ['Africa/Johannesburg', 'Africa/Harare', 'Africa/Maputo'],
-    path: 'M 101,83 L 106,82 L 110,82 L 112,82 L 116,83 L 115,87 L 114,90 L 111,93 L 107,93 L 103,92 L 100,88 L 100,85 L 101,83',
-    labelX: 108,
+    path: 'M 101,82 L 105,82 L 109,82 L 113,83 L 114,86 L 113,90 L 110,93 L 106,92 L 102,91 L 99,88 L 100,84 L 101,82',
+    labelX: 107,
     labelY: 87
   },
 
@@ -232,18 +232,18 @@ const TIMEZONE_REGIONS = [
     name: 'Middle East',
     abbreviation: 'AST/GST',
     timezones: ['Asia/Dubai', 'Asia/Riyadh', 'Asia/Baghdad', 'Asia/Jerusalem', 'Asia/Tehran'],
-    path: 'M 120,64 L 122,62 L 125,60 L 130,60 L 134,60 L 137,63 L 139,67 L 139,70 L 136,73 L 132,74 L 127,73 L 123,70 L 122,67 L 120,64',
+    path: 'M 120,61 L 123,59 L 127,58 L 130,59 L 133,60 L 136,62 L 138,66 L 138,70 L 135,73 L 131,73 L 127,72 L 123,69 L 120,67 L 120,61',
     labelX: 129,
-    labelY: 67
+    labelY: 66
   },
   {
     id: 'central_asia',
     name: 'Central Asia',
     abbreviation: 'ALMT',
     timezones: ['Asia/Tashkent', 'Asia/Almaty', 'Asia/Yekaterinburg'],
-    path: 'M 139,43 L 142,42 L 146,42 L 150,44 L 154,45 L 157,48 L 157,53 L 156,57 L 153,60 L 148,61 L 142,60 L 139,57 L 138,52 L 140,48 L 139,43',
+    path: 'M 135,38 L 139,37 L 144,39 L 148,41 L 152,43 L 155,47 L 155,52 L 154,56 L 151,59 L 147,60 L 142,59 L 138,56 L 137,51 L 138,44 L 135,38',
     labelX: 147,
-    labelY: 52
+    labelY: 49
   },
 
   // South Asia
@@ -252,8 +252,8 @@ const TIMEZONE_REGIONS = [
     name: 'India',
     abbreviation: 'IST',
     timezones: ['Asia/Kolkata', 'Asia/Colombo', 'Asia/Dhaka'],
-    path: 'M 137,63 L 142,60 L 148,61 L 153,63 L 156,66 L 157,70 L 154,73 L 150,75 L 145,75 L 139,74 L 136,73 L 139,70 L 139,67 L 137,63',
-    labelX: 147,
+    path: 'M 138,56 L 142,59 L 147,60 L 151,61 L 154,64 L 155,69 L 152,73 L 148,75 L 143,74 L 138,72 L 135,73 L 138,70 L 138,66 L 136,62 L 136,59 L 138,56',
+    labelX: 146,
     labelY: 67
   },
 
@@ -263,17 +263,17 @@ const TIMEZONE_REGIONS = [
     name: 'Southeast Asia',
     abbreviation: 'ICT/WIB',
     timezones: ['Asia/Bangkok', 'Asia/Jakarta', 'Asia/Singapore', 'Asia/Kuala_Lumpur'],
-    path: 'M 150,75 L 154,73 L 158,73 L 162,74 L 165,76 L 169,76 L 169,79 L 167,82 L 162,83 L 157,83 L 154,81 L 150,78 L 150,75',
-    labelX: 160,
-    labelY: 79
+    path: 'M 148,75 L 152,73 L 156,73 L 159,74 L 162,76 L 165,77 L 167,79 L 166,82 L 162,83 L 157,83 L 153,81 L 149,78 L 148,75',
+    labelX: 158,
+    labelY: 78
   },
   {
     id: 'china',
     name: 'China',
     abbreviation: 'CST',
     timezones: ['Asia/Shanghai', 'Asia/Hong_Kong', 'Asia/Taipei'],
-    path: 'M 153,60 L 156,57 L 157,53 L 157,48 L 160,45 L 165,43 L 171,43 L 176,45 L 180,48 L 180,53 L 179,58 L 176,62 L 171,65 L 165,65 L 160,63 L 156,66 L 153,63 L 153,60',
-    labelX: 168,
+    path: 'M 151,59 L 154,56 L 155,52 L 155,47 L 159,45 L 164,43 L 169,43 L 174,45 L 177,48 L 177,52 L 176,57 L 172,61 L 168,63 L 163,62 L 159,61 L 155,62 L 154,64 L 151,61 L 151,59',
+    labelX: 163,
     labelY: 53
   },
   {
@@ -281,9 +281,9 @@ const TIMEZONE_REGIONS = [
     name: 'Japan & Korea',
     abbreviation: 'JST/KST',
     timezones: ['Asia/Tokyo', 'Asia/Seoul'],
-    path: 'M 179,58 L 180,53 L 180,48 L 183,45 L 187,45 L 190,49 L 190,53 L 189,58 L 185,60 L 180,60 L 179,58',
-    labelX: 185,
-    labelY: 52
+    path: 'M 176,45 L 180,43 L 184,44 L 188,46 L 189,49 L 188,52 L 186,55 L 183,57 L 179,56 L 176,57 L 177,52 L 177,48 L 176,45',
+    labelX: 183,
+    labelY: 49
   },
 
   // Oceania
@@ -292,8 +292,8 @@ const TIMEZONE_REGIONS = [
     name: 'Australia West',
     abbreviation: 'AWST',
     timezones: ['Australia/Perth'],
-    path: 'M 157,83 L 162,83 L 165,85 L 166,88 L 166,91 L 164,94 L 161,94 L 158,93 L 156,89 L 156,86 L 157,83',
-    labelX: 161,
+    path: 'M 154,86 L 157,84 L 160,85 L 162,87 L 163,90 L 162,93 L 159,94 L 155,93 L 154,90 L 154,86',
+    labelX: 159,
     labelY: 89
   },
   {
@@ -301,27 +301,27 @@ const TIMEZONE_REGIONS = [
     name: 'Australia Central',
     abbreviation: 'ACST',
     timezones: ['Australia/Adelaide', 'Australia/Darwin'],
-    path: 'M 165,85 L 169,84 L 172,85 L 175,87 L 175,90 L 174,93 L 170,95 L 166,95 L 164,94 L 166,91 L 166,88 L 165,85',
-    labelX: 170,
-    labelY: 90
+    path: 'M 162,87 L 166,84 L 170,85 L 173,87 L 173,90 L 172,93 L 169,95 L 165,95 L 162,93 L 163,90 L 162,87',
+    labelX: 167,
+    labelY: 89
   },
   {
     id: 'australia_east',
     name: 'Australia East',
     abbreviation: 'AEST',
     timezones: ['Australia/Sydney', 'Australia/Melbourne', 'Australia/Brisbane'],
-    path: 'M 175,87 L 179,85 L 183,86 L 185,88 L 185,92 L 184,95 L 180,96 L 175,95 L 174,93 L 175,90 L 175,87',
-    labelX: 180,
-    labelY: 91
+    path: 'M 173,87 L 177,85 L 180,86 L 183,87 L 184,91 L 182,94 L 178,95 L 174,95 L 172,93 L 173,90 L 173,87',
+    labelX: 179,
+    labelY: 90
   },
   {
     id: 'new_zealand',
     name: 'New Zealand',
     abbreviation: 'NZST',
     timezones: ['Pacific/Auckland', 'Pacific/Chatham'],
-    path: 'M 195,88 L 198,87 L 200,88 L 202,90 L 204,92 L 203,95 L 200,97 L 197,97 L 194,94 L 193,91 L 195,88',
-    labelX: 199,
-    labelY: 92
+    path: 'M 195,86 L 197,85 L 199,85 L 201,87 L 202,91 L 201,94 L 199,96 L 196,95 L 194,93 L 194,89 L 195,86',
+    labelX: 198,
+    labelY: 91
   }
 ];
 
