5c247adc90cd0d2d6a646361f41c12be6fed36d2
Devon Zuegel <devonzuegel@gmail.com>
2026-07-05 16:29:35 -0400

    Revert "Fix lightbox not running: move script to before </body>, run immediately"
    This reverts commit dd243925d349bed0700a9f9e020412f7a89d0fbe.

--- lightbox.js
+++ lightbox.js
@@ -1,4 +1,4 @@
-(function () {
+window.addEventListener('load', function () {
   // Build ordered list of navigable images (exclude .disable-img-click children)
   var allImgs = document.getElementsByTagName('img')
   var navigableImgs = []
