29336a67288ce5b5b76b20b261d93dd25c4be0ac
Devon Zuegel <devonzuegel@gmail.com>
2026-08-01 17:09:41 -0400

    Idle: spacing between round length/scoring sections

--- boggle/sw.js
+++ boggle/sw.js
@@ -2,7 +2,7 @@
    - Precaches app shell for full offline use
    - Queues failed POST /api/boggle calls in IndexedDB, retries on next fetch or sync event
 */
-const CACHE = "boggle-trainer-20260801210918";
+const CACHE = "boggle-trainer-20260801210941";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.svg", "./icon-512.svg"];
 const API_HOST = "boggle-api.vercel.app";
 const QUEUE_DB = "boggle-sync-queue";
