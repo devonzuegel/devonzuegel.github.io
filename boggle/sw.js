3bf1316853fa86a212445f361c2123f0f2228ee5
Devon Zuegel <devonzuegel@gmail.com>
2026-08-01 16:10:46 -0400

    Mobile: board + word list constrained to screen height, right panel scrollable

--- boggle/sw.js
+++ boggle/sw.js
@@ -2,7 +2,7 @@
    - Precaches app shell for full offline use
    - Queues failed POST /api/boggle calls in IndexedDB, retries on next fetch or sync event
 */
-const CACHE = "boggle-trainer-20260801193330";
+const CACHE = "boggle-trainer-20260801201046";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.svg", "./icon-512.svg"];
 const API_HOST = "boggle-api.vercel.app";
 const QUEUE_DB = "boggle-sync-queue";
