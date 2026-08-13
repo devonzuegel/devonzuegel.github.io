139e9e34face66205657515dce682882e76e91c6
Devon Zuegel <devonzuegel@gmail.com>
2026-08-13 10:17:34 -0700

    boggle: deploy 2026-08-13T17:17:34Z

--- boggle/sw.js
+++ boggle/sw.js
@@ -2,7 +2,7 @@
    - Precaches app shell for full offline use
    - Queues failed POST /api/boggle calls in IndexedDB, retries on next fetch or sync event
 */
-const CACHE = "boggle-trainer-20260812223859";
+const CACHE = "boggle-trainer-20260813171734";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.svg", "./icon-512.svg"];
 const API_HOST = "boggle-api.vercel.app";
 const QUEUE_DB = "boggle-sync-queue";
