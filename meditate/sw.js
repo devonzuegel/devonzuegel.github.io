3b4aabf408a44b5e30df19dc8a07dceb1e03455f
Devon Zuegel <devonzuegel@gmail.com>
2026-08-24 00:37:00 -0400

    meditate: show toast on audio error instead of ribbon message

--- meditate/sw.js
+++ meditate/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = 'meditation-shell-20260824043432';
+const SHELL_CACHE = 'meditation-shell-20260824043700';
 const AUDIO_CACHE = 'meditation-audio-v1';
 const SHELL_URLS = ['./', './index.html', './manifest.json'];
 
