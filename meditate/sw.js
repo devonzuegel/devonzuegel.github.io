a50c29eadbb8b84be3014b855dd429783bbe2c18
Devon Zuegel <devonzuegel@gmail.com>
2026-08-24 00:10:11 -0400

    meditate: bigger chevron icons

--- meditate/sw.js
+++ meditate/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = 'meditation-shell-20260824040919';
+const SHELL_CACHE = 'meditation-shell-20260824041011';
 const AUDIO_CACHE = 'meditation-audio-v1';
 const SHELL_URLS = ['./', './index.html', './manifest.json'];
 
