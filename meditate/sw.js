a53e73bb910e77f4108646270dddff381fff0e5f
Devon Zuegel <devonzuegel@gmail.com>
2026-08-24 14:02:36 -0400

    meditate: rename accent color section

--- meditate/sw.js
+++ meditate/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = 'meditation-shell-20260824170425';
+const SHELL_CACHE = 'meditation-shell-20260824180236';
 const AUDIO_CACHE = 'meditation-audio-v1';
 const SHELL_URLS = ['./', './index.html', './manifest.json'];
 
