2b0e15346b96d808157d68ff1b82d45d34f92495
Devon Zuegel <devonzuegel@gmail.com>
2026-08-24 00:08:22 -0400

    meditate: nudge play icon to center optically

--- meditate/sw.js
+++ meditate/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = 'meditation-shell-20260824040746';
+const SHELL_CACHE = 'meditation-shell-20260824040822';
 const AUDIO_CACHE = 'meditation-audio-v1';
 const SHELL_URLS = ['./', './index.html', './manifest.json'];
 
