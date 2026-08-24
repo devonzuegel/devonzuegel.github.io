6c5ac63160fed82f9ab53b161fbdd167188b13dc
Devon Zuegel <devonzuegel@gmail.com>
2026-08-24 14:03:24 -0400

    meditate: move accent color section to bottom of About

--- meditate/sw.js
+++ meditate/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = 'meditation-shell-20260824180257';
+const SHELL_CACHE = 'meditation-shell-20260824180324';
 const AUDIO_CACHE = 'meditation-audio-v1';
 const SHELL_URLS = ['./', './index.html', './manifest.json'];
 
