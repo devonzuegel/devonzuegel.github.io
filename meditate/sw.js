052f7aea9eb78de23cbc73cde294da85804b0b11
Devon Zuegel <devonzuegel@gmail.com>
2026-08-24 00:28:53 -0400

    meditate: fix chevron position on search results to match saved

--- meditate/sw.js
+++ meditate/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = 'meditation-shell-20260824042755';
+const SHELL_CACHE = 'meditation-shell-20260824042853';
 const AUDIO_CACHE = 'meditation-audio-v1';
 const SHELL_URLS = ['./', './index.html', './manifest.json'];
 
