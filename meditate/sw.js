931aecdafe53158a23a7af7955ad335b3d2202f7
Devon Zuegel <devonzuegel@gmail.com>
2026-08-24 14:12:56 -0400

    meditate: deploy 2026-08-24T18:12:56Z

--- meditate/sw.js
+++ meditate/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = 'meditation-shell-20260824181151';
+const SHELL_CACHE = 'meditation-shell-20260824181256';
 const AUDIO_CACHE = 'meditation-audio-v1';
 const SHELL_URLS = ['./', './index.html', './manifest.json'];
 
