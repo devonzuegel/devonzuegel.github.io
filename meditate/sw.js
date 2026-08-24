d097e397bceab342700a9f617516db258ec1b2e0
Devon Zuegel <devonzuegel@gmail.com>
2026-08-24 00:18:11 -0400

    meditate: load more button margin on mobile

--- meditate/sw.js
+++ meditate/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = 'meditation-shell-20260824041712';
+const SHELL_CACHE = 'meditation-shell-20260824041811';
 const AUDIO_CACHE = 'meditation-audio-v1';
 const SHELL_URLS = ['./', './index.html', './manifest.json'];
 
