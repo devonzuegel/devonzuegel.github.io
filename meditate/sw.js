8c2fb9bd1267782f0b68a42644a6114d78bd95b8
Devon Zuegel <devonzuegel@gmail.com>
2026-08-24 00:07:46 -0400

    meditate: use unicode chars for play/pause button instead of SVG

--- meditate/sw.js
+++ meditate/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = 'meditation-shell-20260824040536';
+const SHELL_CACHE = 'meditation-shell-20260824040746';
 const AUDIO_CACHE = 'meditation-audio-v1';
 const SHELL_URLS = ['./', './index.html', './manifest.json'];
 
