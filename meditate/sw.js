03feefe33411e55ec636c0b74dc93cc79b7adf41
Devon Zuegel <devonzuegel@gmail.com>
2026-08-24 13:04:26 -0400

    meditate: accent color picker in About, teal as dark mode default

--- meditate/sw.js
+++ meditate/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = 'meditation-shell-20260824150053';
+const SHELL_CACHE = 'meditation-shell-20260824170425';
 const AUDIO_CACHE = 'meditation-audio-v1';
 const SHELL_URLS = ['./', './index.html', './manifest.json'];
 
