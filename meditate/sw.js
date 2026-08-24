5c80ee1e5b7ad26fbe0ea03185f01c9ede0b2453
Devon Zuegel <devonzuegel@gmail.com>
2026-08-24 14:03:37 -0400

    meditate: trim accent color description

--- meditate/sw.js
+++ meditate/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = 'meditation-shell-20260824180324';
+const SHELL_CACHE = 'meditation-shell-20260824180337';
 const AUDIO_CACHE = 'meditation-audio-v1';
 const SHELL_URLS = ['./', './index.html', './manifest.json'];
 
