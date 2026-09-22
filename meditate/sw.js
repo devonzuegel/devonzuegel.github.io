c59234fdf890713c2fe1be0e78fdc079d474c814
Devon Zuegel <devonzuegel@gmail.com>
2026-09-22 14:37:22 -0400

    meditate: improve library navigation and tracking

--- meditate/sw.js
+++ meditate/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = 'meditation-shell-20260922163852';
+const SHELL_CACHE = 'meditation-shell-20260922183636';
 const AUDIO_CACHE = 'meditation-audio-v1';
 const SHELL_URLS = ['./', './index.html', './manifest.json'];
 
