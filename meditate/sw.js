1f066399d230d5eace175331ef04acf04d3167e1
Devon Zuegel <devonzuegel@gmail.com>
2026-09-22 12:00:21 -0400

    meditate: improve mobile density and scrubbing

--- meditate/sw.js
+++ meditate/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = 'meditation-shell-20260922045124';
+const SHELL_CACHE = 'meditation-shell-20260922155933';
 const AUDIO_CACHE = 'meditation-audio-v1';
 const SHELL_URLS = ['./', './index.html', './manifest.json'];
 
